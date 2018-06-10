75d709b69cc9897be5497ef01cdf902401ffccb8
Mario Moreno <m4mc3r@gmail.com>
2018-06-09 23:24:05 -0300

    Implement Data.Core cleanup

--- src/XGamer.Data.Core/IXGamerDataProvider.cs
+++ src/XGamer.Data.Core/IXGamerDataProvider.cs
@@ -1,12 +1,8 @@
 ﻿using System.Collections.Generic;
-
 using XGamer.Data.Entities;
 
 namespace XGamer.Data.Core
 {
-    /// <summary>
-    /// TODO: Update summary.
-    /// </summary>
     public interface IXGamerDataProvider
     {
         IEnumerable<Game> GetAllGames();
